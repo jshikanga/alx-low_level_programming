90fe3fc3604c055f15dc7a0d207611d110c78e95
jshikanga <zshikanga@gmail.com>
2023-06-22 06:43:05 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+*/
+
+void print_numbers(void)
+{
+	char c'
+	fo (c = '0'; c <= '9'; c++)
+	{
+		_putchar(0);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, followed by
+ * but fot multiples of thress prints Fizz instead of the
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+*/int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz")'
+		} else if (i == 1)
+		{
+			print("%d", i);
+		} else
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+		return (0);
+}
