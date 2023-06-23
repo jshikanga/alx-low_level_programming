9631b8253b67f102f7516bbae623c6ccd241c68c
jshikanga <zshikanga@gmail.com>
2023-06-23 00:16:36 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,20 +5,22 @@
  * @n: number of times the character \ should be printed
 */
 
-void print_diagonal (int n)
+void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
-		int i, j;
-		for (j = 0; j < n; j++)
+		int a, b;
+
+		for (a = 0; a < n; a++)
 		{
-			if (j == i)
 				_putchar('\\');
-			else if (j < i)
+			if (a == b)
+			else if (a < b)
 				_putchar(' ');
 		}
 		_putchar('\n');
+	}
 }
