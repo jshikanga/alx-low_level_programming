ca8579e5ba55a17094fe135a3a177c36ddf86585
jshikanga <zshikanga@gmail.com>
2023-06-23 00:32:21 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,10 +8,16 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-	} else
+
+	}
+
+	else
+
 	{
 		int i, j;
+
 		for (i = 0; i < size; i++)
+			for (j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
