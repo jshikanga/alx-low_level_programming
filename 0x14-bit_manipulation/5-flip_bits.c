45445e84772384e54be74050b64d1b53cdc2fe2c
jshikanga <amumji@gmail.com>
2023-08-03 05:58:28 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -28,3 +28,20 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	}
 	return (count);
 }
+
+/**
+ * get_length - returns the number of bits in a number
+ * @num: number to consider
+ *
+ * Return: length of number
+ */
+unsigned int get_length(unsigned long int num)
+{
+	unsigned int count;
+
+	if (num == 0)
+		return (1);
+	for (count = 0; num != 0; count++)
+		num >>= 1;
+	return (count);
+}
